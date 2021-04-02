d6dac6b6e905cc67a789b0c889f08ff9d2ccf55e
Regis Oliveira <regis.aoc@gmail.com>
2021-04-02 00:26:59 -0300

    mostrando todos os links

--- src/components/LinkImg.jsx
+++ src/components/LinkImg.jsx
@@ -1,48 +1,19 @@
 
 function LinkImg({ product }) {
     const img = product.map(prod => prod.produto.imagem);
-    const img2 = img.map(res => res)
-    console.log('length img:', img.length)
-    console.log('length img2:', img2.length)
-
-
-
-
-    // for (let link of img) {
-    //     console.log('length forlink: ', link.length)
-    //     for (let i = 0; i < link.length; i++) {
-
-    //         return (
-
-    //             <div>{link[i].link}</div>
-
-
-    //         )
-
-    //     }
-    // }
-
-
 
     for (let link of img) {
         console.log('length forlink: ', link.length)
-
-
         return (
-            <ul>
+            <div>
                 {link.map(res =>
-                    <div>
+                    <div key={res.link}>
                         {res.link}
                     </div>
                 )}
-            </ul>
-
-
+            </div>
         )
-
-
-
     }
 }
 
-    export default LinkImg;
+export default LinkImg;
