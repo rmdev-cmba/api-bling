b0b17b648f934b2675d054389a3bb8770c751c95
Regis Oliveira <regis.aoc@gmail.com>
2021-04-02 22:09:45 -0300

    adcionado pasta prod e transferido arquivo teste

--- src/pages/prod/teste.jsx
+++ src/pages/prod/teste.jsx
@@ -1,7 +1,7 @@
 // https://www.youtube.com/watch?v=u1kCtkVR7cE
-import LinkImg from '../components/LinkImg';
-import PrecoVenda from '../components/PrecoVenda';
-import styles from '../styles/Home.module.css'
+import LinkImg from '../../components/LinkImg';
+import PrecoVenda from '../../components/PrecoVenda';
+import styles from '../../styles/Home.module.css'
 
 export default function Teste({ product }) {
   console.log('product:', product)
@@ -41,7 +41,7 @@ export default function Teste({ product }) {
 export async function getServerSideProps() {
   const { API_KEY } = process.env
 
-  let codigo = 4785;
+  let codigo = 4853
   // buscar dados numa api, por exemplo
   const response = await fetch(`https://bling.com.br/Api/v2/produto/${codigo}/json?apikey=${API_KEY}&estoque=S&imagem=S`); // só vai mostar a primeira página buscada, para buscar novas páginas tem que utilizar o método getStaticPaths
   //console.log('response: ', response);
