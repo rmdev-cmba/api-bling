44a460269eef4dd1f7d546e01147f5d300cd339c
Regis Oliveira <regis.aoc@gmail.com>
2021-04-14 22:56:13 -0300

    Incluido mais informações na apresentação do produto pesquisado

--- src/components/PrecoVenda.jsx
+++ src/components/PrecoVenda.jsx
@@ -1,18 +1,29 @@
 
 function PrecoVenda({ pv }) {
     // 'pv' recebe preco de venda
+    const ftd = 0.9 // fator desconto 
+    const descAplicado = ((1-ftd)*100).toFixed(0).replace('.', ',')
+
     const prc = Number(pv)
-    const precoMax = (prc/0.9).toFixed(2).replace('.', ',')
+    const precoMax = (prc/ftd).toFixed(2).replace('.', ',')
+    const parc6 = (prc/ftd/6).toFixed(2).replace('.', ',')
+    const parc3 = (prc/ftd/3).toFixed(2).replace('.', ',')
     const preco = prc.toFixed(2).replace('.', ',')
-    const precoDesc = (prc*0.95).toFixed(2).replace('.', ',')
+    const precoDesc2 = (prc*0.98).toFixed(2).replace('.', ',')
+    const precoDesc4 = (prc*0.96).toFixed(2).replace('.', ',')
+    const precoDesc5 = (prc*0.95).toFixed(2).replace('.', ',')
+    const precoDesc6 = (prc*0.94).toFixed(2).replace('.', ',')
         return (
             <div>
                 <h3>
-                <span>Preço Venda: R$ {precoMax}</span>
-                <span> ------ </span>
-                <span>Promoção: R$ {preco}</span>
-                <span> ------ </span>
-                <span>Desc. Max: R$ {precoDesc}</span>
+                
+                <p>*De R$ {precoMax} em 6x de R$ {parc6} por:</p>
+                <p>*R$ {preco} em 1x ou 3x de R$ {parc3} (desc de {descAplicado}%)</p>
+                <p>*DÉBITO: R$ {precoDesc2} (desc 2%) </p>
+                <span>*DINHEIRO -4%: R$ {precoDesc4} </span>
+                <span>*DIN -5%: R$ {precoDesc5}</span>
+                <span> *DIN -6%: R$ {precoDesc6}</span>
+
                 </h3>
             </div>
         )
--- src/pages/prod/[codigo].jsx
+++ src/pages/prod/[codigo].jsx
@@ -43,18 +43,48 @@ export default function Teste({ product }) {
 
 
 export async function getServerSideProps(context) {
-  const codigo = context.params.codigo
+  const { codigo } = context.params
   const { API_KEY } = process.env
 
+  
+    
+  
   // let codigo = 4853
   // buscar dados numa api, por exemplo
   const response = await fetch(`https://bling.com.br/Api/v2/produto/${codigo}/json?apikey=${API_KEY}&estoque=S&imagem=S`); // só vai mostar a primeira página buscada, para buscar novas páginas tem que utilizar o método getStaticPaths
   //console.log('response: ', response);
   const data = await response.json();
   const product = data.retorno.produtos
+
+// ** inicio tratamento de erros
+  /*
+  const dataError = JSON.stringify(data)
+  const retError = data.retorno.erros
+  const msgError = retError.map(msg => msg)
+  const msg = retError.map(msg => msg.erro)
+
+  
+  const cod = msg[0].cod
+  if(cod==14){
+    
+  }
+  
   //console.log('apikey: ', API_KEY)
-  // console.log('data ', data);
-  console.log(product)
+  
+  console.log('product retornado:', product)
+  console.log('response retornando:', response)
+  console.log('data extraido de response:', data);
+  if (response.status ==200){
+    console.log('encontrado status:', response.status)
+  }
+  console.log('dataError retornado:', dataError)
+  console.log('retError:', retError)
+  console.log('msgError:', msgError)
+  console.log('msg:', msg)
+  // console.log('cod:', cod)
+  */
+  // **fim tratamento erros
+
 
   return {
     props: { product }
