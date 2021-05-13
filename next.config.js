8375f62f53ce420dc21c6e7ca5910c802da3bc46
Regis Oliveira <regis.aoc@gmail.com>
2021-05-12 21:05:07 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@ module.exports = {
     images: {
         deviceSizes: [320, 640, 768, 1024, 1080],
         domains: ['orgbling.s3.amazonaws.com', 'http2.mlstatic.com'],
-    }
+    },
+
+    // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    //     // Note: we provide webpack above so you should not `require` it
+    //     // Perform customizations to webpack config
+    //     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+    
+    //     // Important: return the modified config
+    //     return config
+    //   },
 }
