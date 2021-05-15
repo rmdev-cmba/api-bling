883de9fa64b33e94cf53b5becf6ddb8ef42d105f
Regis Oliveira <regis.aoc@gmail.com>
2021-05-14 23:45:39 -0300

    Update index.tsx

--- src/components/ProductView/index.tsx
+++ src/components/ProductView/index.tsx
@@ -2,14 +2,8 @@ import Image from 'next/image'
 import styles from './ProductView.module.css';
 
 const ProductView = ({ images }) => {
-    // 'imagem' recebe o array 'imagem'
-   
-    // for (let i = 0; i < images.length; i++){
-    //     // console.log('img',i , images[i])
-    // }
-
+    
     const linksImagens = images.map(image => (image.link))
-
 
     return (
         <div className={styles.imgGroup}>
@@ -17,8 +11,9 @@ const ProductView = ({ images }) => {
             <div>
                 <Image 
                 src={linksImagens[0]}
-                width={450}
-                height={450}
+                width={420}
+                height={420}
+                objectFit="contain"
                  />
             </div>
             
